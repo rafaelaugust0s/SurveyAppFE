7dfb4379d6fd26fc972c0c2ca7e2e8661c8b6ade
rafaelsuarez <rafaelaugustosc@gmail.com>
2021-05-07 18:17:53 -0400

    Front end components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from "./componets/commons/Header";
+import Footer from "./componets/commons/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App =()=>{
+
+return(
+
+    <div class="container text-center">
+        <div row= "row">
+            <div col-12>
+                <Header/>
+            </div>
+        </div>
+        <div row= "row">
+            <div col-12>
+                <h1>This is body</h1>
+            </div>
+        </div>
+
+        <div row= "row">
+            <div col-12>
+                <Footer/>
+            </div>
+        </div>
     </div>
-  );
+)
 }
 
-export default App;
+export default App
